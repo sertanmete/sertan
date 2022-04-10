b2cca2d6b588e17ceb15e39191f38a4842a0ab31
sertanmete <sertanmete@gmail.com>
2022-04-10 16:12:57 +0200

    ilk gosterihhm

--- src/test/java/mete/sar.java
+++ src/test/java/mete/sar.java
@@ -4,6 +4,7 @@ public class sar {
     public static void main(String[] args){
         System.out.println("dondurma");
         System.out.println("dondurma sade");
+        System.out.println("dondurma kakao");
 
 
 
